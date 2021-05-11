eb316f5ce8dd8e0ae422a00c480b143657daedd3
Junmo Park <junmopark@Junmo-Parkui-MacBookPro.local>
2021-05-11 19:59:33 +0900

    초기 세팅 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import Main from './Pages/Main/Main';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import Main from './Pages/Main/Main';
+import Category from './Pages/Category/Category';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/Login/Signup';
+import Product from './Pages/Product/Product';
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/main" component={Main} />
+          <Route exact path="/category" component={Category} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/product" component={Product} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
       </Router>
     );
   }
 }
-
 export default Routes;
