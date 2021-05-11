6b9a0abd244da6c2a1ea01aff285855492c007b0
하연주 <llilyha123@gmail.com>
2021-05-11 21:29:36 +0900

    Modify:클래스명 수정, 레이아웃 작업 중

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,70 +1,123 @@
 import React, { Component } from 'react';
 
+import './Product.scss';
+
 export class Product extends Component {
   render() {
     return (
       <div className="product">
-        <article id="kakaoBody">
+        <nav></nav>
+        <section className="productContents">
           전체 페이지(리뷰, 문의까지)
-          <div id="productWrap">
+          <div className="productDetail">
             디테일 페이지 (리뷰 위에까지만)
-            <div className="container">
-              콘텐츠 있는 부분만(마진값 있음) ::before
-              <div className="row">
+            <div className="productDetailOutbox">
+              이미지 콘텐츠 있는 부분만(마진값 있음) ::before
+              <div className="productDetailInnerbox">
                 상위 태그 동일 ::before
-                <div className="col product-content">
+                <div className="productDetailImagesOutbox">
                   상세 정보 있는 부분만(오른쪽 sticky빼고 )(패딩 값 있음)
-                  {/* product thumbnail */}
-                  <div
-                    id="productRollingBanner"
-                    className="product-header-rolling-banner"
-                  >
-                    썸네일 이미지
+                  <div className="productDetailImages">
+                    {/* product thumbnail */}
+                    <div className="productThumnail">
+                      <img
+                        className="productThumnailImg"
+                        alt="(상품이름) 이미지"
+                        src=""
+                      ></img>
+                    </div>
                   </div>
-                  {/* product floating */}
-                  <aside id="prodOptFloating" className="prodOpt-fixed">
-                    오른쪽 고정 옵션 창
-                    <div className="product-options">
-                      상위태그 동일(마진값만 있음)
-                      {/* product floating header*/}
-                      <header className="heading">
-                        옵션창 heading 부분
-                        <p className="category" id="goodsCategory">
-                          굿즈 카테고리 넣기
-                        </p>
-                        <h1 className="product-name" id="goodsName">
-                          굿즈 이름(레디 미니 파우치 -어피치)
-                        </h1>
-                        <p className="price">
-                          가격(discount 적용할 수 있는 태그들 숨겨져있음)
-                        </p>
-                        <ul className="event">
-                          list들 css플랙스의 연속~~
-                          <li className="savingCash">
-                            <span className="title">리스트 타이틀</span>
-                            <span className="content">리스트 콘텐츠</span>
-                          </li>
-                          <li className="deliveryFee">
-                            <span className="title">리스트 타이틀</span>
-                            <span className="content">리스트 콘텐츠</span>
-                          </li>
-                        </ul>
-                      </header>
-                      <div className="quantity">
-                        <span class="title">수량</span>
-                        <span className="quantity-group">
-                          <button class="minus">-</button>
-                          <input></input>
-                          <button class="plus">+</button>
-                        </span>
+                </div>
+                {/* product floating */}
+                <aside className="FloatingOptionOutbox">
+                  <div className="FloatingOptionInnerbox">
+                    {/* product floating header~~~~~~*/}
+                    <header className="productOptionHeading">
+                      <p className="ProductCategory">굿즈 카테고리</p>
+                      <h1 className="productName">
+                        굿즈 이름(레디 미니 파우치 -어피치)
+                      </h1>
+                      <p className="ProductPrice">
+                        가격(discount 적용할 수 있는 태그들 숨겨져있음)
+                      </p>
+                      <ul>
+                        list들 css플랙스의 연속~~
+                        <li className="saveCash">
+                          <span className="title">리스트 타이틀</span>
+                          <span className="content">리스트 콘텐츠</span>
+                        </li>
+                        <li className="deliveryFee">
+                          <span className="title">리스트 타이틀</span>
+                          <span className="content">리스트 콘텐츠</span>
+                        </li>
+                      </ul>
+                    </header>
+                    {/* ~~~~~~product floating header*/}
+                    {/* product floating Amount~~~~~*/}
+                    <div className="productQuantity">
+                      <span>수량</span>
+                      <span className="quantityGroup">
+                        <button className="minus">-</button>
+                        <input
+                          className="countNum"
+                          type="text"
+                          maxlength="2"
+                          value="1"
+                        />
+                        <button className="plus">+</button>
+                      </span>
+                    </div>
+                    {/* ~~~~product floating Amount*/}
+                    {/* product floating options~~~~*/}
+                    <div className="options">
+                      <ul>
+                        <li>옵션1</li>
+                        <li>옵션2</li>
+                      </ul>
+                    </div>
+                    {/* ~~~~product floating options*/}
+                    {/* product floating pay  ~~~~ */}
+                    <div className="payOutbox">
+                      <div className="payInnerbox">
+                        <div className="textLeft">
+                          <strong>__</strong>개 상품 금액
+                        </div>
+                        <div className="textRight">
+                          <strong>__</strong>원
+                        </div>
                       </div>
                     </div>
-                  </aside>
-                </div>
+                    {/* ~~~~~product floating pay */}
+                    {/* product floating bottomBtns  ~~~~ */}
+                    <div className="optionButtonsOutbox">
+                      <div className="optionButtons">
+                        <div>
+                          좋아요 버튼
+                          <a>
+                            <input className="likeCheckbox" type="checkbox" />
+                            <label htmlFor="likeButton">
+                              <span>하트 아이콘</span>
+                              좋아요
+                            </label>
+                          </a>
+                        </div>
+                        <div>
+                          장바구니 버튼
+                          <a>장바구니</a>
+                        </div>
+                        <div>
+                          구매하기 버튼
+                          <a>구매하기</a>
+                        </div>
+                      </div>
+                    </div>
+                    {/* ~~~~~ product floating bottomBtns*/}
+                  </div>
+                </aside>
               </div>
             </div>
           </div>
-        </article>
+        </section>
       </div>
     );
   }
