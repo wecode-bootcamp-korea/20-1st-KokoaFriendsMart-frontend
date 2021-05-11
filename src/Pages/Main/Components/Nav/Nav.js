0f65c1f21795166b4f40e901bf66e9f52fbf6485
Chris <wychrischo@gmail.com>
2021-05-11 11:25:20 +0900

    Finished Nav Bar html mark up

--- src/Pages/Main/Components/Nav/Nav.js
+++ src/Pages/Main/Components/Nav/Nav.js
@@ -2,7 +2,147 @@ import React, { Component } from 'react';
 
 export class Nav extends Component {
   render() {
-    return <div>hi</div>;
+    return (
+      <div className="main">
+        <div className="mainContainer">
+          <div className="subContainer">
+            <div className="navBarLeftContainer">
+              <ul className="navBarLeft">
+                <li className="navBarHover">
+                  <a href="/#">카테고리</a>
+                  <div className="navDropdown">
+                    <div className="dropdownContainer">
+                      <div className="allItems">
+                        <div className="allItemsTitle">
+                          <a href="/#">전체</a>
+                        </div>
+                      </div>
+                      <div className="elecItems">
+                        <div className="elecItemsTitle">
+                          <a href="/#">일렉트로닉스</a>
+                        </div>
+                        <ul className="elecItemsSubCategories">
+                          <li>
+                            <a href="/#">아이맥</a>
+                          </li>
+                          <li>
+                            <a href="/#">태블릿</a>
+                          </li>
+                          <li>
+                            <a href="/#">폰</a>
+                          </li>
+                          <li>
+                            <a href="/#">워치</a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="wearItems">
+                        <div className="wearItemsTitle">
+                          <a href="/#">웨어</a>
+                        </div>
+                        <ul className="wearItemsSubCategories">
+                          <li>
+                            <a href="/#">셔츠</a>
+                          </li>
+                          <li>
+                            <a href="/#">모자</a>
+                          </li>
+                          <li>
+                            <a href="/#">후드</a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="livingItems">
+                        <div className="livingItemsTitle">
+                          <a href="/#">리빙</a>
+                        </div>
+                        <ul className="livingItemsSubCategories">
+                          <li>
+                            <a href="/#">컵</a>
+                          </li>
+                          <li>
+                            <a href="/#">북</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="navBarBest">
+                  <a href="/#">베스트</a>
+                </li>
+              </ul>
+            </div>
+            <div className="navBarHeader">
+              <a href="/#">Kakao Friends Mart</a>
+            </div>
+            <div className="navBarRightContainer">
+              <ul className="navBarRight">
+                <li className="navBarSearch">
+                  <div className="formGroup">
+                    <div className="searchContainer">
+                      <input className="searchInput" type="text" />
+                      <a href="/#" className="searchButtonIcon">
+                        <i class="fas fa-search" />
+                      </a>
+                    </div>
+                  </div>
+                  <div className="searchTrend">
+                    <div className="topSearch">
+                      <h4>인기검색어</h4>
+                      <ol>
+                        <li>
+                          <a href="/#">아이맥</a>
+                        </li>
+                        <li>
+                          <a href="/#">폰</a>
+                        </li>
+                        <li>
+                          <a href="/#">셔츠</a>
+                        </li>
+                        <li>
+                          <a href="/#">태블릿</a>
+                        </li>
+                        <li>
+                          <a href="/#">후드</a>
+                        </li>
+                      </ol>
+                    </div>
+                    <div className="recentSearch">
+                      <div className="searchResults">
+                        <h4>최근 검색어</h4>
+                        <div className="searchList">최근 검색어가 없습니다</div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="navBarUserInfo">
+                  <a href="/#">
+                    <i class="far fa-user" />
+                  </a>
+                  <div className="navBarUserSubPage">
+                    <ul>
+                      <li>
+                        <a href="/#" className="UserSubPageLogin">
+                          로그인
+                        </a>
+                      </li>
+                      <li>주문내역</li>
+                      <li>좋아요</li>
+                    </ul>
+                  </div>
+                </li>
+                <li className="navBarMyCart">
+                  <a href="/#">
+                    <i class="fas fa-shopping-basket" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './Pages/Main/Main';
+// import Category from './Pages/Category/Category';
+// import Login from './Pages/Login/Login';
+// import Signup from './Pages/Login/Signup';
+// import Product from './Pages/Product/Product';
 
 class Routes extends React.Component {
   render() {
@@ -8,6 +12,10 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/main" component={Main} />
+          {/* <Route exact path="/category" component={Category} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/product" component={Product} />
+          <Route exact path="/signup" component={Signup} /> */}
         </Switch>
       </Router>
     );
