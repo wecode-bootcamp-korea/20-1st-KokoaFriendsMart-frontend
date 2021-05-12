b48a2207c85f380377bbe3656d9fb60fb975f178
Chris <wychrischo@gmail.com>
2021-05-12 19:53:40 +0900

    Add: nav bar still working on friends map

---
+++ src/Pages/Main/Components/Nav/CharactersList/CharactersList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Characters from '../Characters/Characters';
+import './CharactersList.scss';
+
+class CharactersList extends Component {
+  render() {
+    const { friends } = this.props;
+    return (
+      <div className="CharacterList">
+        {friends.map(friends => {
+          return <Characters img={friends.img} name={friends.name} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default CharactersList;
--- src/Pages/Main/Components/Nav/Nav.js
+++ src/Pages/Main/Components/Nav/Nav.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import CharacterList from './CharactersList/CharactersList';
 import './Nav.scss';
 
-export class Nav extends Component {
+class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+    };
+  }
+
+  // 데이터 로딩
+  componentDidMount() {
+    fetch('/public/data/character.json')
+      .then(res => res.json())
+      .then(res => this.state({ friends: res }));
+  }
+
   render() {
     return (
       <div className="Nav">
@@ -16,6 +31,36 @@ export class Nav extends Component {
                       <div className="allItems">
                         <div className="allItemsTitle">
                           <a href="/#">전체</a>
+                        </div>
+                        <div className="dropdownCharacters">
+                          <div className="dropdownCharactersTitle">
+                            프랜즈별
+                          </div>
+                          <CharacterList friends={this.state.friends} />
+                          {/* <ul>
+                            <li>
+                              <img
+                                className="friendsPic"
+                                src="http://localhost:3000/images/trunk.jpg"
+                                alt="/#"
+                                width="70"
+                                height="70"
+                              />
+                              <span className="tree">춘삼이</span>
+                            </li>
+                            <li>
+                              <span className="Peach">피치피치</span>
+                            </li>
+                            <li>
+                              <span className="Duck">덕이</span>
+                            </li>
+                            <li>
+                              <span className="Dino">콘콘</span>
+                            </li>
+                            <li>
+                              <span className="Bird">삐약이</span>
+                            </li>
+                          </ul> */}
                         </div>
                       </div>
                       <div className="elecItems">
@@ -86,33 +131,37 @@ export class Nav extends Component {
                       <a href="/#" className="searchButtonIcon">
                         <i class="fas fa-search" />
                       </a>
-                    </div>
-                  </div>
-                  <div className="searchTrend">
-                    <div className="topSearch">
-                      <h4>인기검색어</h4>
-                      <ol>
-                        <li>
-                          <a href="/#">아이맥</a>
-                        </li>
-                        <li>
-                          <a href="/#">폰</a>
-                        </li>
-                        <li>
-                          <a href="/#">셔츠</a>
-                        </li>
-                        <li>
-                          <a href="/#">태블릿</a>
-                        </li>
-                        <li>
-                          <a href="/#">후드</a>
-                        </li>
-                      </ol>
-                    </div>
-                    <div className="recentSearch">
-                      <div className="searchResults">
-                        <h4>최근 검색어</h4>
-                        <div className="searchList">최근 검색어가 없습니다</div>
+                      <div className="searchAndRecentContainer">
+                        <div className="searchTrend">
+                          <div className="topSearch">
+                            <h4>인기검색어</h4>
+                            <ol>
+                              <li>
+                                <a href="/#">아이맥</a>
+                              </li>
+                              <li>
+                                <a href="/#">폰</a>
+                              </li>
+                              <li>
+                                <a href="/#">셔츠</a>
+                              </li>
+                              <li>
+                                <a href="/#">태블릿</a>
+                              </li>
+                              <li>
+                                <a href="/#">후드</a>
+                              </li>
+                            </ol>
+                          </div>
+                        </div>
+                        <div className="recentSearch">
+                          <div className="searchResults">
+                            <h4>최근 검색어</h4>
+                            <div className="searchList">
+                              최근 검색어가 없습니다
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -124,12 +173,20 @@ export class Nav extends Component {
                   <div className="navBarUserSubPage">
                     <ul>
                       <li>
-                        <a href="/#" className="UserSubPageLogin">
+                        <a href="/#" className="userSubPageLogin">
                           로그인
                         </a>
                       </li>
-                      <li>주문내역</li>
-                      <li>좋아요</li>
+                      <li>
+                        <a href="/#" className="userSubPageOrder">
+                          주문내역
+                        </a>
+                      </li>
+                      <li>
+                        <a href="/#" className="userSubPageOrder">
+                          좋아요
+                        </a>
+                      </li>
                     </ul>
                   </div>
                 </li>
