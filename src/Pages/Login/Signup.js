f449adc6234ddc2ae0fd3b92faefb4bb304195fa
Junmo Park <junmopark@Junmo-Parkui-MacBookPro.local>
2021-05-13 00:24:22 +0900

    Add: config 파일 추가

--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { loginSignupApi } from '../../config';
 import './Signup.scss';
 
 class Signup extends Component {
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const loginSignupApi = 'http://10.58.2.175:9000';
