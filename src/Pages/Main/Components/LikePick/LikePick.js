297331da4902e677e63c40bc0665fd34a7303e2b
Chris <wychrischo@gmail.com>
2021-05-16 14:47:01 +0900

    Added characters

--- src/Pages/Main/Components/LikePick/LikePick.js
+++ src/Pages/Main/Components/LikePick/LikePick.js
@@ -1,8 +1,22 @@
 import reactDom from 'react-dom';
 import React, { Component } from 'react';
+import CharactersList from '../../Components/Nav/CharactersList/CharactersList';
 import './LikePick.scss';
 
 class LikePick extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('/data/character.json')
+      .then(res => res.json())
+      .then(res => this.setState({ friends: res }));
+  }
+
   render() {
     return (
       <div className="LikePick">
@@ -10,7 +24,9 @@ class LikePick extends Component {
           <div className="headerContainer">
             <h2 className="title">Like Pick!</h2>
           </div>
-          <div className="characterContainer">{/* character map */}</div>
+          <div className="characterContainer">
+            <CharactersList friends={this.state.friends} />
+          </div>
           <div className="itemContainer">{/* component mapping */}</div>
         </div>
       </div>
