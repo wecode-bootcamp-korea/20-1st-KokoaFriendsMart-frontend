f905b64712d2a03c5154310f254c46efa714e5cc
Chris <wychrischo@gmail.com>
2021-05-16 14:12:40 +0900

    Updated header css

--- src/Pages/Main/Components/LikePick/LikePick.js
+++ src/Pages/Main/Components/LikePick/LikePick.js
@@ -6,11 +6,13 @@ class LikePick extends Component {
   render() {
     return (
       <div className="LikePick">
-        <div className="headerContainer">
-          <h2 className="title">Like Pick!</h2>
+        <div className="likePickContainer">
+          <div className="headerContainer">
+            <h2 className="title">Like Pick!</h2>
+          </div>
+          <div className="characterContainer">{/* character map */}</div>
+          <div className="itemContainer">{/* component mapping */}</div>
         </div>
-        <div className="characterContainer">{/* character map */}</div>
-        <div className="itemContainer">{/* component mapping */}</div>
       </div>
     );
   }
