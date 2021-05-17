c95acd2c9b2339672b7b8396336972b3784a9233
하연주 <hayeonju@hayeonjuui-MacBookAir.local>
2021-05-18 01:44:00 +0900

    Add:장바구니 레이아웃 구현 완료, 목데이터 적용

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,68 +1,83 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Nav from '../Main/Components/Nav/Nav';
+import ProductInCart from '../Cart/Components/ProductInCart';
 import './Cart.scss';
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cartProductData: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('/data/cartProductData.json')
+      .then(res => res.json())
+      .then(cartProductData => {
+        this.setState({
+          cartProductData: cartProductData,
+        });
+      });
+  }
+
   render() {
+    const { cartProductData } = this.state;
+    // console.log('카트', cartProductData);
     return (
       <div className="cart">
+        <Nav />
+        {/* <Banner /> */}
+
         <section className="cartPageOutbox">
+          <article className="topBanner">
+            <div className="cartDescription">
+              <p className="cart">장바구니</p>
+              <p>주문하실 상품 및 수량을 한번 더 확인해 주세요.</p>
+            </div>
+
+            <img
+              alt="코코아프랜즈들"
+              src="/images/characterImages/concon.png"
+            />
+          </article>
           <article className="freeDeliveryBar">
             <span>무료배송 까지</span>
             {/* 만약 장바구니 가격이 5만원 넘었다면 ??끝까지 가고 0원으로뜸 & 그 밑에 가격표시 */}
-            <div class="percentageBar">
+
+            <div className="totalBar">
+              <div class="percentageBarLeft"></div>
+              <div className="percentageBarRight"></div>
               <img
-                className="character"
+                className="movingCharacter"
                 alt="캐릭터"
                 src="/images/characterImages/peach.png"
               />
               <div className="movingPrice">
-                <span>{'총 가격'}</span>원
+                <span>총 가격</span>원
               </div>
             </div>
+
             <span className="price"> (5만원 - 담은가격)원</span>
           </article>
 
           <article className="checkBox">
             <div className="checkBoxHeader">
               <input className="check" type="checkBox" />
-              <span className="choice">전체선택</span>
-              <span>(선택개수/총개수)</span>
-              <span>옵션</span>
-              <span>수량</span>
-              <span>상품금액</span>
-              <a>전체 삭제</a>
+              <span className="totalChoice">전체선택</span>
+              <span className="choiceNum">(선택개수/총개수)</span>
+              <span className="choiceOption">옵션</span>
+              <span className="choiceQuantity">수량</span>
+              <span className="productTotal">상품금액</span>
+              <a className="deleteAll">전체삭제</a>
             </div>
-            {/* 아래 맵 돌릴 부분 */}
-            <div className="cartProductBox">
-              <input type="checkBox" />
-              <img
-                alt="상품 이미지"
-                src="https://i.ibb.co/zrrbfgW/Electronics-Imac-Concon-1.png"
-              />
-              <div className="productNamePrice">
-                <p>(상품 이름)</p>
-                <p>(상품가격(고정))</p>
-              </div>
-
-              <div className="productOption">
-                <div className="productQuantity">
-                  <span className="quantityGroup">
-                    <button className="minus">-</button>
-                    <input
-                      className="countNum"
-                      type="text"
-                      maxlength="2"
-                      value="1"
-                    />
-                    <button className="plus">+</button>
-                  </span>
-                </div>
-                <span>{'상품 가격'}원</span>
-                <i class="fas fa-trash"></i>
-              </div>
-            </div>
-            {/* 위까지 맵 돌릴 부분 */}
+            {/* 상품 목록 맵 */}
+            {cartProductData.map((cartProduct, index) => {
+              return (
+                <ProductInCart key={cartProduct.id} cartProduct={cartProduct} />
+              );
+            })}
           </article>
           <article className="totalPriceText">
             <div>상품금액</div>
