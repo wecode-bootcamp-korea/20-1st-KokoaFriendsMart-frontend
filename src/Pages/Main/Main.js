f674cbc3acd82b9d54035bd435ab83286a4b77e3
Junmo Park <junmopark@Junmo-Parkui-MacBookPro.local>
2021-05-18 14:04:17 +0900

    Add: footer 추가

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Nav from './Components/Nav/Nav';
+import Footer from '../Footer/Footer';
 
 export class Main extends Component {
   render() {
     return (
       <div>
-        <Nav></Nav>
+        <Nav />
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Category from './Pages/Category/Category';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Login/Signup';
 import Product from './Pages/Product/Product';
+import Footer from './Pages/Footer/Footer';
 
 class Routes extends React.Component {
   render() {
@@ -21,6 +22,7 @@ class Routes extends React.Component {
           <Route exact path="/product" component={Product} />
           <Route exact path="/signup" component={Signup} />
         </Switch>
+        <Footer />
       </Router>
     );
   }
