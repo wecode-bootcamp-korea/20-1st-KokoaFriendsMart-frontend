3046c41eb3249d31c9ef31cfaa1c77792109d6f3
Junmo Park <junmopark@Junmo-Parkui-MacBookPro.local>
2021-05-18 20:00:41 +0900

    Add: 초기 레이아웃 완료

--- src/Pages/Orderlist/Orderlist.js
+++ src/Pages/Orderlist/Orderlist.js
@@ -1,8 +1,71 @@
 import React, { Component } from 'react';
+import './Orderlist.scss';
 
 export class Orderlist extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="orderlist">
+        <div className="wraper header">
+          <div className="mypageHeader">
+            <div className="basicInfo userInfo">
+              <div className="imgWrap">
+                <img alt="profile" src="https://i.ibb.co/1mNL56P/ducky.png" />
+              </div>
+              <p className="largeText name">박준모님</p>
+              <p>junmopark01@gmail.com</p>
+            </div>
+            <div className="rank userInfo">
+              <p className="smallText">등급</p>
+              <p className="largeText">일반회원</p>
+            </div>
+            <div className="coupon userInfo">
+              <p className="smallText">쿠폰</p>
+              <p className="largeText">0</p>
+            </div>
+            <div className="cash userInfo">
+              <p className="smallText">캐시</p>
+              <p className="largeText">2000</p>
+            </div>
+          </div>
+        </div>
+        <div className="wraper">
+          <main className="orderInfo">
+            <div className="myInfo">
+              <div>
+                <p className="boldText">쇼핑 정보</p>
+                <p className="lightText">주문내역</p>
+                <p className="lightText">교환/반품</p>
+                <p className="lightText">상품 리뷰</p>
+                <p className="lightText">좋아요</p>
+              </div>
+              <div>
+                <p className="boldText">회원 정보</p>
+                <p className="lightText">쿠폰</p>
+                <p className="lightText">캐시 & 포인트</p>
+                <p className="lightText">회원정보수정</p>
+              </div>
+              <div>
+                <p className="boldText">문의내역</p>
+                <p className="lightText">1:1 문의</p>
+                <p className="lightText">상품 Q&A</p>
+              </div>
+            </div>
+            <div className="detail">
+              <div className="orderDetail">
+                <div>
+                  <p>주문내역상세</p>
+                </div>
+                <div className="orderList">
+                  <div className="date">2021.05.21</div>
+                  <div className="product"></div>
+                </div>
+              </div>
+              <div className="paymentDetail"></div>
+            </div>
+          </main>
+        </div>
+      </div>
+    );
   }
 }
 
