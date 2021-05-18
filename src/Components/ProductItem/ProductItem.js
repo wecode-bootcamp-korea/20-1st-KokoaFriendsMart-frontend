4c2ce7d6ed34e394ac28b3561614902bfe64cfa3
Chris <wychrischo@gmail.com>
2021-05-18 20:13:15 +0900

    still working on likepick

--- src/Components/ProductItem/ProductItem.js
+++ src/Components/ProductItem/ProductItem.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './ProductItem.scss';
-
 class ProductItem extends Component {
   render() {
-    const { list, link } = this.props;
+    const { list, link, size } = this.props;
     return (
-      <div className="ProductItem" key={list.id}>
-        <div className="item">
+      <div className="ProductItem">
+        <div className={`item ${size}`}>
           <Link to={link} className="thum">
             <div>
               <img
                 src={list.thumbnail_url}
                 alt="이미지 섬네일"
-                className="productImg"
+                className={`productImg ${size}`}
               />
             </div>
             <span className={`label ${list.is_sale ? 'sale' : 'hide'}`}>
@@ -28,16 +27,15 @@ class ProductItem extends Component {
             <span className={`label ${list.is_new ? 'new' : 'hide'}`}>NEW</span>
           </Link>
           <div className="itemDesc">
-            <div className="itemTitle">{list.name}</div>
+            <div className={`itemTitle ${size}`}>{list.name}</div>
             <i
               className={`fa-heart ${list.is_liked ? 'fas yellow' : 'far'}`}
             ></i>
-            <div className="itemPrice">{list.price}원</div>
+            <div className={`itemPrice ${size}`}>{list.price}원</div>
           </div>
         </div>
       </div>
     );
   }
 }
-
 export default ProductItem;
